743fda2bec7e30c383f0a1be1057f3117aaf4a9a
vitaly <vitalijdietrich@gmail.com>
2024-05-03 02:12:06 +0200

    added a new Grid Button where the user can then enter a Number and a new Grid will be created in the same space of the container

--- script.js
+++ script.js
@@ -1,27 +1,64 @@
 
 
 const container = document.querySelector(".container");
+const buttonContainer = document.querySelector("#buttonContainer");
+const newGridButton = document.createElement("button");
 
-for (let i = 0; i<Math.pow(16, 2); i++) {
-    const div = document.createElement("div");
-    container.appendChild(div);
-    div.classList.add("rows");
-    
+
+buttonContainer.appendChild(newGridButton);
+newGridButton.textContent = "Create New Grid"
+
+
+initialGrid();
+mouseOverDiv();
+
+
+function initialGrid () {
+
+    for (let i = 0; i<Math.pow(16, 2); i++) {
+        const div = document.createElement("div");
+        container.appendChild(div);
+        div.classList.add("rows");
+    }
 }
 
 
-let divs = container.querySelectorAll(".rows"); 
 
-
-divs.forEach(div => {
-div.addEventListener("mouseover", mouseOverDiv);
+newGridButton.addEventListener("click", () => {
+    let userInput = prompt("Enter a Grid Number: ");
+    userInput = Number(userInput);
+    if (userInput > 100) {
+        return alert ("100 is the max. Enter a lower Number");
+    }
+    if (userInput) {
+        newGrid(userInput);
+        mouseOverDiv();
+    }
 });
 
-function mouseOverDiv() {
-    this.style.backgroundColor = "red";
+
+function newGrid (userInput) {
+    
+    let divs = container.querySelectorAll(".rows"); 
+    divs.forEach(div => {container.removeChild(div);})
+    
+    for (let j = 0; j < Math.pow(userInput, 2); j++) {
+        const div = document.createElement("div");
+        container.appendChild(div);
+        div.classList.add("rows");
+    }
+
+
 }
 
 
+function mouseOverDiv() {
 
+let divs = container.querySelectorAll(".rows"); 
+divs.forEach(div => {
+div.addEventListener("mouseover", () => {
+    div.style.backgroundColor = "red";
+});
+});
 
-
+} 
