2900d5c1c76cd2fff12330d9199a8b0870c7a7b4
vitaly <vitalijdietrich@gmail.com>
2024-05-03 16:57:50 +0200

    once you hover over the divs they change colors and the opacity gets lower by 10 percent each time like the assignment said

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ const container = document.querySelector(".container");
 const buttonContainer = document.querySelector("#buttonContainer");
 const newGridButton = document.createElement("button");
 
-
 buttonContainer.appendChild(newGridButton);
 newGridButton.textContent = "Create New Grid"
 
 
 initialGrid();
 mouseOverDiv();
-
 
 function initialGrid () {
 
@@ -28,21 +26,18 @@ function initialGrid () {
 newGridButton.addEventListener("click", () => {
     let userInput = prompt("Enter a Grid Number: ");
     userInput = Number(userInput);
+
     if (userInput > 100) {
         return alert ("100 is the max. Enter a lower Number");
-    }
-    if (userInput) {
-        newGrid(userInput);
-        mouseOverDiv();
-    }
+    }   if (userInput) {
+            newGrid(userInput);
+            mouseOverDiv();
+    
+        }
 });
 
-
-
-
-
 function newGrid (userInput) {
-    exampleVariable = 100 / userInput; 
+    const gridLayout = 100 / userInput; 
     let divs = container.querySelectorAll(".rows"); 
     divs.forEach(div => {container.removeChild(div);})
     
@@ -50,19 +45,36 @@ function newGrid (userInput) {
         const div = document.createElement("div");
         container.appendChild(div);
         div.classList.add("rows");
-        div.style.flex = `0 1 calc(${exampleVariable}% - 1.82px)`;
-    }
-   
+        div.style.flex = `0 1 calc(${gridLayout}% - 1.82px)`;
+    }  
 }
 
+function incrementVariable (number) {
+    number -= 0.1;
+    return number = parseFloat(number.toFixed(1));
+    }
 
 function mouseOverDiv() {
 
-let divs = container.querySelectorAll(".rows"); 
-divs.forEach(div => {
-div.addEventListener("mouseover", () => {
-    div.style.backgroundColor = "red";
-});
-});
+let opacityValue = 1;
 
-} 
+let divs = container.querySelectorAll(".rows"); 
+    divs.forEach(div => {
+        div.addEventListener("mouseenter", () => {
+           
+                    let red = Math.floor(Math.random()*255);
+                    let green = Math.floor(Math.random()*255);
+                    let blue = Math.floor(Math.random()*255);
+                    div.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+                    opacityValue = incrementVariable(opacityValue)
+                    div.style.opacity = opacityValue;
+                    console.log(opacityValue);
+        });
+    });
+}  
+
+ 
+
+
+
+  
