d4fbf1e8f1eca17fb7740a40eca2c31355531e1e
vitaly <vitalijdietrich@gmail.com>
2024-06-05 12:27:06 +0200

    cleaned up some code

--- script.js
+++ script.js
@@ -18,14 +18,12 @@ let currentColor = '#d946ef';
 
 function blueColorBtn() {
     blueColorButton.addEventListener("click", () => {
-
         currentColor = '#6366f1';
     })
 }
 
 function redColorBtn() {
     redColorButton.addEventListener("click", () => {
-
         currentColor = '#f472b6';
     })
 }
@@ -76,31 +74,12 @@ function mouseOverDiv() {
 
 let opacityValue = 1;
 
-/* let divs = container.querySelectorAll(".rows"); 
-    divs.forEach(div => {
-        div.addEventListener("mouseenter", () => {
-           
-                    let red = Math.floor(Math.random()*255);
-                    let green = Math.floor(Math.random()*255);
-                    let blue = Math.floor(Math.random()*255);
-                    div.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-                    opacityValue = incrementVariable(opacityValue)
-                    
-                    console.log(opacityValue);
-        });
-    });
-}  
- */
+
 
 let divs = container.querySelectorAll(".rows"); 
     divs.forEach(div => {
         div.addEventListener("mouseenter", () => {
-           
-                    
-                    div.style.backgroundColor = currentColor;
-                 
-                    
-               
+                    div.style.backgroundColor = currentColor;      
         });
     });
 }  
